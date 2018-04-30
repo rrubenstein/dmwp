d9decb0b62e1c0087040a2f8f8df674d239ce807
Student <student@Ravi-Rubensteins-MacBook-Pro.local>
2018-04-30 15:20:45 -0400

    finished the 3rd coding challenge in chapter 7 in the Calc.py file

--- Calc.py
+++ Calc.py
@@ -44,3 +44,44 @@ def verify_cont():
         print('{0} is not continuous at {1}'.format(f, p))
     else:
         print('didnt work')
+
+def grad_decent(x0, f1x, x):
+    epsilon = 1e-6
+    step_size = 1e-4
+    x_old = x0
+    x_new = x_old - step_size*f1x.subs({x:x_old}).evalf()
+    while abs(x_old + x_new) > epsilon:
+        x_old = x_new
+        x_new = x_old - step_size*f1x.subs({x:x_old}).evalf()
+    return x_new
+
+def gradiant():
+    f = raw_input('Enter a function in one variable: ')
+    var = raw_input('Enter the variable to differentiate with respect to: ')
+    var0 = float(raw_input('Enter the initial value of the variable: '))
+    try:
+        f = sympify(f)
+    except SympifyError:
+        print('oops')
+    else:
+        var = Symbol(var)
+        d = Derivative(f, var).doit()
+        var_min = grad_decent(var0, d, var)
+        print('{0}: {1}'.format(var.name, var_min))
+        print('Minimum value: {0}'.format(f.subs({var:var_min})))
+
+def intergal_of_curve(f, var, start, finish):
+    var = Symbol(var)
+    integ = Integral(f, (var, start, finish)).doit()
+    return integ
+
+def area_bet_curve():
+    f1 = raw_input('Enter the upper function: ')
+    f2 = raw_input('Enter the lower function: ')
+    var = raw_input('Enter the variable: ')
+    a = float(raw_input('Enter the starting point: '))
+    b = float(raw_input('Enter the ending point: '))
+    fx1 = intergal_of_curve(f1, var, a, b)
+    fx2 = intergal_of_curve(f2, var, a, b)
+    area = fx1 - fx2
+    print('The area between the functions {0} and {1} between {2} and {3} is {4}'.format(f1, f2, a, b, area))
