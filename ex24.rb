7b622bc0171c30e0d450ef0ac1738b9cbf1c5684
Student <student@Ravi-Rubensteins-MacBook-Pro.local>
2018-05-10 11:47:29 -0400

    compleated exersises 21 through the 26 test. moving on in ruby book

---
+++ ex24.rb
@@ -0,0 +1,13 @@
+def secret_formula(started)
+  jelly_beans = started * 500
+  jars = jelly_beans / 1000
+  crates = jars / 100
+  return jelly_beans, jars, crates
+end
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+puts "With a starting point of: #{start_point}"
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
+start_point = start_point / 10
+puts "We can also do that this way:"
+puts "We'd have %s beans, %d jars, and %d crates." % secret_formula(start_point)
